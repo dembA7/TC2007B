e45810d4435c780d7bceb92648e20fc94ba91c96
unknown <a01709522@tec.mx>
2023-09-15 13:41:30 -0600

    add splashscreen and app themes

--- app/src/main/java/com/example/kotlin/mypokedexapp/utils/Constants.kt
+++ app/src/main/java/com/example/kotlin/mypokedexapp/utils/Constants.kt
@@ -3,4 +3,5 @@ package com.example.kotlin.mypokedexapp.utils
 object Constants {
     const val BASE_URL = "https://pokeapi.co/api/v2/"
     const val MAX_POKEMON_NUMBER = 1279
+    const val SPLASHCREEN_DURATION = 5000L
 }
