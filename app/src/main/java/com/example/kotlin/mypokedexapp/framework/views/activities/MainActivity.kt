9274323ed6e72e7870d8ce76eb51439eb5035f17
unknown <a01709522@tec.mx>
2023-09-15 15:38:41 -0600

    add onClick event to search fragment

--- app/src/main/java/com/example/kotlin/mypokedexapp/framework/views/activities/MainActivity.kt
+++ app/src/main/java/com/example/kotlin/mypokedexapp/framework/views/activities/MainActivity.kt
@@ -8,11 +8,14 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.kotlin.mypokedexapp.R
 import com.example.kotlin.mypokedexapp.framework.views.fragments.PokedexFragment
+import com.example.kotlin.mypokedexapp.framework.views.fragments.SearchFragment
+import com.example.kotlin.mypokedexapp.utils.Constants
 
 class MainActivity: AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var currentFragment: Fragment
+    private var currentMenuOption:String?= null
 
     private val viewModel: MainViewModel by viewModels()
 
@@ -21,7 +24,8 @@ class MainActivity: AppCompatActivity() {
 
         initializeBinding()
         initializeObservers()
-        exchangeCurrentFragment(PokedexFragment())
+        initializeListeners()
+        exchangeCurrentFragment(PokedexFragment(), Constants.MENU_POKEDEX)
 
     }
 
@@ -34,11 +38,34 @@ class MainActivity: AppCompatActivity() {
 
     }
 
-    private fun exchangeCurrentFragment(newFragment:Fragment){
+    private fun initializeListeners(){
+        binding.appBarMain.llPokedex.setOnClickListener {
+            selectMenuOption(Constants.MENU_POKEDEX)
+        }
+
+        binding.appBarMain.llSearch.setOnClickListener {
+            selectMenuOption(Constants.MENU_SEARCH)
+        }
+    }
+
+    private fun selectMenuOption(menuOption:String){
+        if(menuOption == currentMenuOption){
+            return
+        }
+
+        when(menuOption){
+            Constants.MENU_POKEDEX -> exchangeCurrentFragment(PokedexFragment(),Constants.MENU_POKEDEX)
+            Constants.MENU_SEARCH -> exchangeCurrentFragment(SearchFragment(),Constants.MENU_SEARCH)
+        }
+    }
+
+    private fun exchangeCurrentFragment(newFragment: Fragment, newMenuOption:String){
         currentFragment = newFragment
 
         supportFragmentManager.beginTransaction()
             .replace(R.id.nav_host_fragment_content_main,currentFragment)
             .commit()
+
+        currentMenuOption = newMenuOption
     }
 }
